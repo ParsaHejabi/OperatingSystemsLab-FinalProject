76b89dd57323ce1cb3f354ed5eac2af242d526b1
Parsa Hejabi <parsa.hejabi@gmail.com>
2020-01-19 13:34:54 +0330

    thread.h is refactored using new priority queue. priority member is deleted from thread struct.

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -2,7 +2,7 @@
 #define THREADS_THREAD_H
 
 #include <debug.h>
-#include <list.h>
+#include <priorityQueue.h>
 #include <stdint.h>
 
 /* States in a thread's life cycle. */
@@ -87,11 +87,11 @@ struct thread
   enum thread_status status; /* Thread state. */
   char name[16];             /* Name (for debugging purposes). */
   uint8_t *stack;            /* Saved stack pointer. */
-  int priority;              /* Priority. */
-  struct list_elem allelem;  /* List element for all threads list. */
+  /* Priority is embedded in priorityQueue_elem. */
+  struct priorityQueue_elem allelem;  /* priority queue element for all threads priority queue. */
 
   /* Shared between thread.c and synch.c. */
-  struct list_elem elem; /* List element. */
+  struct priorityQueue_elem elem; /* priority queue element. */
 
 #ifdef USERPROG
   /* Owned by userprog/process.c. */
